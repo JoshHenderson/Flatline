ae8f113a15b51f0d6bd35de8e5b22eb731d22b03
Josh Henderson <henderson.josh1@gmail.com>
2016-06-30 11:22:39 -0700

    Hierarchy changes

--- Flatline/Requests/RequestFrameworkProtocol.swift
+++ Flatline/Requests/RequestFrameworkProtocol.swift
@@ -6,9 +6,4 @@
 //  Copyright © 2016 Joshua Henderson. All rights reserved.
 //
 
-@objc (FLRequestFrameworkProtocol) public protocol RequestFrameworkProtocol {
-    public func executeRequestWithHTTPMethod(httpMethod: RequestMethod!,
-                                            urlString: String!,
-                                            parameters: ([String:String!])?,
-                                            completion: RequestManagerClosureType)
-}
+import Foundation
