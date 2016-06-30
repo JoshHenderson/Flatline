50c0b00a9579c90babd60ae8cdeed635335e3562
Josh Henderson <henderson.josh1@gmail.com>
2016-06-30 11:15:37 -0700

    Initial Commit

---
+++ Flatline/Flatline.h
@@ -0,0 +1,19 @@
+//
+//  Flatline.h
+//  Flatline
+//
+//  Created by Joshua Henderson on 6/30/16.
+//  Copyright © 2016 Joshua Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flatline.
+FOUNDATION_EXPORT double FlatlineVersionNumber;
+
+//! Project version string for Flatline.
+FOUNDATION_EXPORT const unsigned char FlatlineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flatline/PublicHeader.h>
+
+
